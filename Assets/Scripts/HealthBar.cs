6c899c5a7baffaf8752d21665eba15adccc07d8f
13Samael04 <Ratmir.vakhitov@gmail.com>
2022-11-25 23:19:15 +0300

    new

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Life _life;
+
+    private float _speedChang = 10f;
+    private float _minSliderValue = 0f;
+    private float _maxSliderValue = 100f;
+    private Slider _slider;
+    private Coroutine _changeHealth;
+
+    private void OnEnable()
+    {
+        _life.LifeBar += StartChange;
+    }
+
+    private void OnDisable()
+    {
+        _life.LifeBar -= StartChange;
+    }
+
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+        _slider.value = Mathf.Clamp(_slider.value, _minSliderValue, _maxSliderValue);
+    }
+
+    public void StartChange(float targetValue)
+    {
+        if(_changeHealth != null)
+        {
+            StopCoroutine(_changeHealth);
+        }
+
+        _changeHealth = StartCoroutine(ChangeHealth(targetValue));
+    }
+
+    private IEnumerator ChangeHealth(float targetValue)
+    {
+        while (_slider.value != targetValue)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, targetValue, _speedChang * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Life : MonoBehaviour
+{
+    public event UnityAction<float> LifeBar;
+    private float _minHealth = 0f;
+    private float _maxHealth = 100f;
+
+    public float Health { get; private set; } = 100f;
+
+    public void Attack(float changeHealth)
+    {
+        ChangeHealth(changeHealth);
+    }
+
+    public void Heal(float changeHealth)
+    {
+        ChangeHealth(changeHealth);
+    }
+
+    private void ChangeHealth(float cangeIndex)
+    {
+        Health += cangeIndex;
+        Health = Mathf.Clamp(Health, _minHealth, _maxHealth);
+        LifeBar?.Invoke(Health);
+        print(Health);
+    }
+}
